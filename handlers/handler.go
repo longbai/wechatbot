f9b08c55733da86efdc6c3674ec5fd3e80099e54
Bai Long <slongbai@gmail.com>
2023-03-21 00:02:56 +0800

    fixed @

--- handlers/handler.go
+++ handlers/handler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+    "github.com/longbai/wechatbot/config"
     "log"
 
     "github.com/eatmoreapple/openwechat"
@@ -33,23 +34,25 @@ func Handler(msg *openwechat.Message) {
     log.Printf("hadler Received msg : %v", msg.Content)
     // 处理群消息
     if msg.IsSendByGroup() {
-        //_ = handlers[GroupHandler].handle(msg)
+        _ = handlers[GroupHandler].handle(msg)
+        return
+    }
+
+    if msg.IsSendByFriend() {
+        _ = handlers[UserHandler].handle(msg)
         return
     }
 
     // 好友申请
     if msg.IsFriendAdd() {
         return
-        //if config.LoadConfig().AutoPass {
-        //    _, err := msg.Agree("你好我是基于chatGPT引擎开发的微信机器人，你可以向我提问任何问题。")
-        //    if err != nil {
-        //        log.Fatalf("add friend agree error : %v", err)
-        //        return
-        //    }
-        //}
+        if config.LoadConfig().AutoPass {
+            _, err := msg.Agree("你好我是基于chatGPT引擎开发的微信机器人，你可以向我提问任何问题。")
+            if err != nil {
+                log.Fatalf("add friend agree error : %v", err)
+                return
+            }
+        }
     }
-    if msg.IsSendByFriend() {
-        _ = handlers[UserHandler].handle(msg)
-        return
-    }
+
 }
