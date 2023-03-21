2f50b61a19152480c165d3df1a4d35805ab4239a
Bai Long <slongbai@gmail.com>
2023-03-22 07:56:10 +0800

    use openai go lib

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/longbai/wechatbot
 
 go 1.20
 
-require github.com/eatmoreapple/openwechat v1.4.1
+require (
+	github.com/eatmoreapple/openwechat v1.4.1
+	github.com/sashabaranov/go-openai v1.5.4
+)
 
 replace github.com/eatmoreapple/openwechat => ../openwechat
--- gpt/gpt.go
+++ gpt/gpt.go
@@ -1,16 +1,15 @@
 package gpt
 
 import (
-    "bytes"
-    "encoding/json"
-    "io"
-    "log"
-    "net/http"
+    "context"
+    "fmt"
+
+    "github.com/sashabaranov/go-openai"
 
     "github.com/longbai/wechatbot/config"
 )
 
-const BASEURL = "https://api.openai.com/"
+const Backend = "https://api.openai.com/"
 
 // ChatGPTResponseBody 请求体
 type ChatGPTResponseBody struct {
@@ -46,88 +45,38 @@ type ChatGPTRequestBody struct {
     Messages         []RoleContent `json:"messages,omitempty"`
 }
 
-// Completions gtp文本模型回复
-//curl https://api.openai.com/v1/completions
-//-H "Content-Type: application/json"
-//-H "Authorization: Bearer your chatGPT key"
-//-d '{"model": "text-davinci-003", "prompt": "give me good song", "temperature": 0, "max_tokens": 7}'
 func Completions(msg string) (string, error) {
-    requestBody := ChatGPTRequestBody{
-        Model:            "gpt-3.5-turbo",
-        Temperature:      0.8, // 每次返回的答案的相似度0-1（0：每次都一样，1：每次都不一样）
-        MaxTokens:        4090 - len(msg)*2,
-        TopP:             1,
-        FrequencyPenalty: 0,
-        PresencePenalty:  0.6,
-        Stop:             []string{" Human:", " AI:"},
-    }
-    if config.LoadConfig().Model != "" {
-        requestBody.Model = config.LoadConfig().Model
-    }
-    if requestBody.Model == "text-davinci-003" {
-        requestBody.Prompt = msg
-    } else {
-        requestBody.Messages = []RoleContent{
-            {"user", msg},
-        }
-    }
+    cfg := openai.DefaultConfig(config.LoadConfig().ApiKey)
+    backend := Backend
 
-    requestData, err := json.Marshal(requestBody)
-
-    if err != nil {
-        return "", err
-    }
-    log.Printf("request gtp json string : %v", string(requestData))
-    base := BASEURL
     if config.LoadConfig().Backend != "" {
-        base = config.LoadConfig().Backend
+        backend = config.LoadConfig().Backend
     }
-    req, err := http.NewRequest("POST", base+"v1/chat/completions", bytes.NewBuffer(requestData))
+    cfg.BaseURL = backend + "v1"
+    client := openai.NewClientWithConfig(cfg)
+
+    resp, err := client.CreateChatCompletion(
+        context.Background(),
+        openai.ChatCompletionRequest{
+            Model: openai.GPT3Dot5Turbo,
+            Messages: []openai.ChatCompletionMessage{
+                {
+                    Role:    openai.ChatMessageRoleUser,
+                    Content: msg,
+                },
+            },
+            Temperature:      0.8,
+            TopP:             1,
+            Stop:             []string{" Human:", " AI:"},
+            PresencePenalty:  0.6,
+            FrequencyPenalty: 0,
+        },
+    )
+
     if err != nil {
+        fmt.Printf("ChatCompletion error: %v\n", err)
         return "", err
     }
 
-    apiKey := config.LoadConfig().ApiKey
-    req.Header.Set("Content-Type", "application/json")
-    req.Header.Set("Authorization", "Bearer "+apiKey)
-    client := &http.Client{}
-    response, err := client.Do(req)
-    if err != nil {
-        return "", err
-    }
-
-    defer response.Body.Close()
-
-    body, err := io.ReadAll(response.Body)
-    if err != nil {
-        log.Printf("read response body error : %s, %v", string(body), err)
-        return "", err
-    }
-
-    if response.StatusCode != http.StatusOK {
-        log.Printf("request gtp error : %s %s", response.Status, string(body))
-        return "", err
-    }
-
-    gptResponseBody := &ChatGPTResponseBody{}
-    log.Println(string(body))
-    err = json.Unmarshal(body, gptResponseBody)
-    if err != nil {
-        return "", err
-    }
-    var reply string
-    if len(gptResponseBody.Choices) > 0 {
-        for _, v := range gptResponseBody.Choices {
-            if v["text"] != nil {
-                reply = v["text"].(string)
-            } else if v["message"] != nil {
-                v2 := v["message"]
-                msg := v2.(map[string]interface{})
-                reply = msg["content"].(string)
-            }
-            break
-        }
-    }
-    //log.Printf("gpt response text: %s \n", reply)
-    return reply, nil
+    return resp.Choices[0].Message.Content, nil
 }
